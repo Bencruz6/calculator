e0093bc2425c3bfba8fa4ddc519c2a57ffc83065
Benher <198418227+Ben-Code-One@users.noreply.github.com>
2025-07-30 13:16:36 +0530

    fixed bugs so that the user cannot enter multiple operators

--- cal.js
+++ cal.js
@@ -1,7 +1,7 @@
 const add = (op1, op2) => op1 + op2;
 const subtract = (op1, op2) => op1 - op2;
 const multiply = (op1, op2) => op1 * op2;
-const divide = (op1, op2) => op2 !== 0 ? op1 / op2 : 'Cannot divide by zero';
+const divide = (op1, op2) => op2 !== 0 ? op1 / op2 : NaN;
 
 let operand1 = "", operand2 = "", operator = "";
 
@@ -33,7 +33,43 @@ const operate = (op1, op, op2) => {
 const buttons = document.getElementsByClassName("num");
 let disp = document.getElementById("sc");
 let dispTxt = "";
+let result = null;
 //console.log(buttons)
+
+
+
+
+
+
+
+//function that evaluates
+
+const Eval = () => {
+
+    if (operand1 && operand2 && operator) {
+        result = operate(parseFloat(operand1), operator, parseFloat(operand2));
+        console.log("the computation is", result);
+        disp.innerText = result
+
+        operand1 = result;
+        operand2 = "";
+        operator = ""
+    }
+
+    else {
+
+
+        
+            alert("please enter tho numbers to perform arithmetic magic")
+        
+    }
+
+
+}
+
+
+
+
 
 for (let i = 0; i < buttons.length; ++i) {
     buttons[i].addEventListener("click", function () {
@@ -47,7 +83,24 @@ for (let i = 0; i < buttons.length; ++i) {
             operand2 += value;  // Assign value to operand2
         }
 
-    });
+        //checking if it can evaluate
+
+        // if (operand1 && operand2 && operator) {
+
+
+        //     Eval();
+        //     if (result) {
+        //         operand2 = "";
+        //         operator = ""
+        //         operand1=result
+
+        //     }
+
+
+        // }
+
+    }
+    )
 }
 
 
@@ -56,15 +109,19 @@ const ops = document.getElementsByClassName("op");
 for (let i = 0; i < ops.length; ++i) {
     ops[i].addEventListener("click", function () {
         const value = ops[i].value;
-        
-        if (operand1 !== "") {
-            operator += value;
+
+        if (operand1 !== "" && operator==="") {
+            operator = value;
             disp.innerText += value;
-            
+
 
         }
-        else {
-            alert("You have to enter a number first!");
+        else if(operator){
+            alert("You have already entered an operator  '"+ operator +"'  "+ "please don't enter an onather one");
+        }
+
+        else{
+            alert("Enter a number first")
         }
 
 
@@ -75,37 +132,28 @@ for (let i = 0; i < ops.length; ++i) {
 
 //evaluate
 
-const equal =document.getElementById("evaluate");
+const equal = document.getElementById("evaluate");
 
-if(equal){
-    equal.addEventListener("click",function(){
+if (equal) {
+    equal.addEventListener("click", Eval)
 
-        if(operand1 && operand2  && operator){
-            const result = operate(parseInt(operand1),operator,parseInt(operand2));
-            console.log("the computation is",result);
-            disp.innerText=result
-        }
 
-        else{
-            alert("please enter tho numbers to perform arithmetic magic")
-        }
 
-    })
 }
 
 
 
 //clear
 
-const clear =document.getElementById("clr");
+const clear = document.getElementById("clr");
 
-if(clear){
-    clear.addEventListener("click", function(){
-        disp.innerText="";
-        dispTxt="";
-        operand1="";
-        operand2="";
-        operator="";
+if (clear) {
+    clear.addEventListener("click", function () {
+        disp.innerText = "";
+        dispTxt = "";
+        operand1 = "";
+        operand2 = "";
+        operator = "";
     });
 }
 
